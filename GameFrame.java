31fc7b53687ae10f77b5e970f837d90e6984667d
Harout Mardirossian <harout@sygnoos.com>
2020-04-10 20:06:51 +0400

    Game Engine - base functionality

--- GameFrame.java
+++ GameFrame.java
@@ -6,7 +6,96 @@ import java.awt.image.DataBufferInt;
 import java.util.ArrayList;
 import javax.swing.JFrame;
 
-public class Game extends JFrame implements Runnable
+public class GameFrame extends JFrame implements Runnable
 {
+	private static final long serialVersionUID = 1L;
 
+	// game map params
+	public int mapWidth = 15;
+	public int mapHeight = 15;
+
+	private Thread gameEngineThread;
+	private boolean running; // does engine runs
+	private BufferedImage imageBuffer;
+
+	public int[] pixels;
+
+	/**
+	 * Game map overview from top
+	 *
+	 * 0 - empty space
+	 * >= 1 - texture on the wall
+	 *
+	 */
+	public static int[][] gameMapOverview = {
+		{1,1,1,1,1,1,1,1,2,2,2,2,2,2,2},
+		{1,0,0,0,0,0,0,0,2,0,0,0,0,0,2},
+		{1,0,3,3,3,3,3,0,0,0,0,0,0,0,2},
+		{1,0,3,0,0,0,3,0,2,0,0,0,0,0,2},
+		{1,0,3,0,0,0,3,0,2,2,2,0,2,2,2},
+		{1,0,3,0,0,0,3,0,2,0,0,0,0,0,2},
+		{1,0,3,3,0,3,3,0,2,0,0,0,0,0,2},
+		{1,0,0,0,0,0,0,0,2,0,0,0,0,0,2},
+		{1,1,1,1,1,1,1,1,4,4,4,0,4,4,4},
+		{1,0,0,0,0,0,1,4,0,0,0,0,0,0,4},
+		{1,0,0,0,0,0,1,4,0,0,0,0,0,0,4},
+		{1,0,0,2,0,0,1,4,0,3,3,3,3,0,4},
+		{1,0,0,0,0,0,1,4,0,3,3,3,3,0,4},
+		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,4},
+		{1,1,1,1,1,1,1,4,4,4,4,4,4,4,4}
+	};
+
+	public GameFrame()
+	{
+		// Open a new empty gameEngineThread
+		gameEngineThread = new Thread(this);
+		imageBuffer = new BufferedImage(640, 480, BufferedImage.TYPE_INT_RGB); // set the image buffer for texturing
+		pixels = ((DataBufferInt)imageBuffer.getRaster().getDataBuffer()).getData(); // get image buffer data
+
+		// start the engine
+		setJframeConfigs();
+		startEngine();
+	}
+
+	private void setJframeConfigs()
+	{
+		setSize(640, 480);
+		setResizable(false);
+		setTitle("3D Engine");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBackground(Color.black);
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+
+	private void startEngine()
+	{
+		// set engine status as runnging
+		running = true;
+		gameEngineThread.start();
+	}
+
+	/**
+	 * Stop thread and terminat engine
+	 *
+	 * must be - synchronized
+	 */
+	public synchronized void stop()
+	{
+		// set engine status as runnging
+		running = true;
+
+		try {
+			// wait for thread complited
+			gameEngineThread.join();
+		}
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void run()
+	{
+
+	}
 }
