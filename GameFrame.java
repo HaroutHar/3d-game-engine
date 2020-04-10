bd99b5826356853b663bb67b8d59789e7ef371fa
Harout Mardirossian <harout@sygnoos.com>
2020-04-10 20:36:55 +0400

    Texturing

--- GameFrame.java
+++ GameFrame.java
@@ -3,7 +3,6 @@ import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
-import java.util.ArrayList;
 import javax.swing.JFrame;
 
 public class GameFrame extends JFrame implements Runnable
