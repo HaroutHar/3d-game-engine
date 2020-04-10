8a1924c151cfdcf891d13223862847eab2163250
Harout Mardirossian <harout@sygnoos.com>
2020-04-10 19:35:05 +0400

    Initial Commit

---
+++ GameFrame.java
@@ -0,0 +1,12 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import java.util.ArrayList;
+import javax.swing.JFrame;
+
+public class Game extends JFrame implements Runnable
+{
+
+}
